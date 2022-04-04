83f29d45fdaa9120a3c21a7e02dc836dff31338d
gadogado092 <gadogado092>
2022-04-04 14:12:37 +0800

    show data from room to list

--- app/src/main/java/com/example/inventory/ItemListFragment.kt
+++ app/src/main/java/com/example/inventory/ItemListFragment.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.inventory.databinding.ItemListFragmentBinding
@@ -29,6 +30,13 @@ import com.example.inventory.databinding.ItemListFragmentBinding
  * Main fragment displaying details for all items in the database.
  */
 class ItemListFragment : Fragment() {
+
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database
+                .itemDao()
+        )
+    }
 
     private var _binding: ItemListFragmentBinding? = null
     private val binding get() = _binding!!
@@ -50,6 +58,16 @@ class ItemListFragment : Fragment() {
                 getString(R.string.add_fragment_title)
             )
             this.findNavController().navigate(action)
+        }
+
+        val adapter = ItemListAdapter {
+        }
+        binding.recyclerView.adapter = adapter
+
+        viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
+            items.let {
+                adapter.submitList(it)
+            }
         }
     }
 }
--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -2,6 +2,7 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 @Entity
 data class Item(
@@ -14,3 +15,6 @@ data class Item(
     @ColumnInfo(name = "quantity")
     val quantityInStock: Int
 )
+
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)
